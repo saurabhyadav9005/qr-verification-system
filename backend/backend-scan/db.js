3e402fa12e7b7e98bee40e239fd61815363c8c98
saurabhyadav9005 <71484078+saurabhyadav9005@users.noreply.github.com>
2026-01-18 11:47:01 +0530

    Update db.js
    server cred updated

--- backend/backend-scan/db.js
+++ backend/backend-scan/db.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
   host: 'localhost',
-  user: 'qr_user',
-  password: 'qr_pass123',
-  database: 'qr_verification',
+  user: 'scanqr',
+  password: 'scanVerify@123',
+  database: 'qr_verify',
   waitForConnections: true,
   connectionLimit: 10
 });
